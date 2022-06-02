a29744cdc600277f4e968ca634e493eaf8e77b29
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-06-02 09:25:22 -0700

    hello puts

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*prints out text using the puts
+*returns 0
+*/
+
+int main(void)
+{
+char str1[] = "Programming is like building a multilingual puzzle"
+puts(str1);
+return (0);
+}
