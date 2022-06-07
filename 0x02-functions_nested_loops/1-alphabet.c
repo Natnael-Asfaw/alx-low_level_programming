da3f714bc01a26bc526f007f6cc35f20dfd10698
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-06-07 11:53:02 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - prints alphabet
+*in lowercase
+*/
+void print_alphabet(void)
+{
+char n;
+n = 'a';
+while (n <= 'z')
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
