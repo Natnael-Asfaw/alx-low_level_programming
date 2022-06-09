7f8cb9d64475bc6f3dcbc8abbe800e8434dd46f7
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-06-09 05:24:14 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,9 +5,9 @@
 *@n: int to be multi to i
 *Return: resultof the operation
 */
-int mul(int i, int n)
+int s(int i, int n)
 {
-int mul;
-mul = i + n;
-return (mul);
+int s;
+s = i + n;
+return (s);
 }
