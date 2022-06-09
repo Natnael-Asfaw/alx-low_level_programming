7a1df459567a5cb4e9a2e47131fa442ebf07d4e1
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-06-09 05:20:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - multiples two integers
+*@i: int to be multiplied to n
+*@n: int to be multi to i
+*Return: resultof the operation
+*/
+int mul(int i, int n)
+{
+int mul;
+mul = i + n;
+return (mul);
+}
