46d2744ad30f5963d52df5b3c392ad82e3bce575
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-06-13 10:51:59 -0700

    Resets the first value of a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,7 @@
  *reset_to_98 - reset the first value of a viriabel to 98
  *@n: is a pointer
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-int i;
-int *n;
-i = 402;
-n = i;
-printf("n=%d\n", *n);
 *n = 98;
-printf("n=%d\n", *n);
 }
