f1265bd7b9416f82173f0207c4179f5a330e9c1d
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-07-05 10:29:48 -0700

    a progrsm that prints the name of the file it was compiled from, followed by a \n

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,12 @@
 
 /**
  * main - program that prints the name of the file it was
- *  compiled from followed \n
+ *  compiled from followed by \n
  *  Return: 0
  */
 
 int main(void)
 {
-printf("%s\n", _FILE_);
+printf("%s\n",--FILE--);
 return (0);
 }
