b4610785008c037b586b1eec02ed2c5fecc2513b
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-07-15 05:42:30 -0700

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,13 @@
 size_t print_list(const list_t *h)
 {
 size_t n = 0;
-while (h)
+for (h = 0; h++)
 {
 if (!h->str)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-n++;
 }
 return (n);
 }
