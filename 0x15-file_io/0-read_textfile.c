29fedc055e08f805a84338d73dc2aeac3991a4f4
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-07-27 02:52:23 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - a function that reads a text file and prints 
+ * read_textfile - a function that reads a text file and prints
  * it to the POSIX standard output.
  * @filename: ponter
  * @letters: size letters
@@ -13,28 +11,40 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t file, i, n;
-char *text;
-text = malloc(sizeof(letters));
+char *buf;
 
-if (text == NULL)
-return (0);
-
-if (filename == NULL)
+if (!filename)
 return (0);
 
 file = open(filename, O_RDONLY);
 
 if (file == -1)
+return (0);
+
+buf = malloc(sizeof(char) * letters);
+
+if (!buf)
+return (0);
+
+i = read(file, buf, letters);
+
+if (i == -1)
 {
-free(text);
+free(buf);
 return (0);
 }
 
-i = read(file, text, letters);
-n = write(STDOUT_FILEND, text, i);
+buf[i] = '\0';
 
 close(file);
 
-return (n);
+n = write(STDOUT_FILEND, buf, i);
+if (n == -1)
+{
+free(buf);
+return (0);
 }
 
+free(buf);
+return (n);
+}
