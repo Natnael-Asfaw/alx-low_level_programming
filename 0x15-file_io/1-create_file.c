19a17db5d55aa1d7ff05f103fe404a1f1da6a41f
Natnael-Asfaw <natnaelsimonasfaw@gmail.com>
2022-07-29 02:18:17 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file -creates a file and fills it with text
  * @filename: name of the file to create
  * @text_content: text to write in the file
- * Return: 1 on success else -1 
+ * Return: 1 on success else -1
  */
 
 int create_file(const char *filename, char *text_content)
